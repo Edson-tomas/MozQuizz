6a01d075f9e49b2ec78b1eb7bebaaa06cd224ce8
Edson-tomas <edsonn89@gmail.com>
2018-06-10 22:06:20 +0200

    Removed the buttons that were being used to redirect/open new activities; Addition of new onclick methods on FirstPageActivity.class; Linking the onlcik methods with the textviews on activity_first_page.xml Rearranged the code;

--- app/src/main/java/com/example/etomas/mozquizz/FirstPageActivity.java
+++ app/src/main/java/com/example/etomas/mozquizz/FirstPageActivity.java
@@ -1,7 +1,9 @@
 package com.example.etomas.mozquizz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstPageActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class FirstPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+    }
+
+    public void onAddQuestionsClick(View view) {
+
+        Intent activityIntent = new Intent(this, AddingQuestionActivity.class);
+        startActivity(activityIntent);
+
+    }
+
+    public void onPlayClick(View view) {
+
+        Intent activityIntent = new Intent(this, AddingQuestionActivity.class);
+        startActivity(activityIntent);
+
+    }
+
+    public void onPlayerRankingClick(View view) {
+
+        Intent activityIntent = new Intent(this, AddingQuestionActivity.class);
+        startActivity(activityIntent);
+
     }
 }
